58dbcbc0b5ca8cdbc99bfe78f4f31ba19c213806
eunmo <eunmo@cs.stanford.edu>
2018-05-07 14:40:37 +0900

    Add blur effect to status bar

--- Tango Add/AppDelegate.swift
+++ Tango Add/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if let statusbar = UIApplication.shared.value(forKey: "statusBar") as? UIView {
+            let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
+            let blurView = UIVisualEffectView(effect: blurEffect)
+            blurView.frame = statusbar.bounds
+            blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            statusbar.addSubview(blurView)
+            statusbar.sendSubview(toBack: blurView)
+        }
+        
         return true
     }
 
